33e5d149acfadc0cc72bfaff88ed467fe21873a7
ashok jadhav <ashok.jadhav@armstrongltd.in>
2023-12-17 17:05:37 +0530

    Add project files.

---
+++ MVCTestWebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCTestWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
